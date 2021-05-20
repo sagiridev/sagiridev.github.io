b1d2c377d9e42d6bdbfd02d9c27e4f406d39bb4a
Nightscape Coffee <coffeebank@catdev.anonaddy.com>
2021-05-20 06:38:05 -0700

    Add discord invite

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,8 +2,11 @@
     <Layout>
 
       <div class="w-full pt-4 md:pt-8">
-        <div class="w-4/5 md:w-1/2 mx-auto p-4 text-center text-gray-800 bg-white rounded-lg shadow-lg md:shadow-xl">
+        <div class="w-4/5 md:w-1/2 mx-auto p-4 text-center text-gray-800 bg-white rounded-lg shadow-lg md:shadow-xl showsecret1">
           <h1 class="my-12 md:my-16 font-bold font-mono text-2xl md:text-3xl">Coming soon</h1>
+          <div class="opacity-0 showsecret2 -mt-10 md:-mt-12 mb-2 md:mb-4 transition-all duration-300">
+            <a class="text-indigo-800 font-mono text-xs md:text-sm" href="https://coffeebank.github.io/discord">Curious?</a>
+          </div>
         </div>
         <div class="pt-8 text-center">
           <a class="text-gray-200 font-mono text-sm" href="https://www.deviantart.com/greenmapple17/art/Sagiri-Izumi-Eromanga-Sensei-663945059" target="_blank" rel="noopener">Art by greenmapple17</a>
@@ -20,3 +23,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.showsecret1:hover > .showsecret2 {
+  opacity: 1 !important;
+}
+</style>
